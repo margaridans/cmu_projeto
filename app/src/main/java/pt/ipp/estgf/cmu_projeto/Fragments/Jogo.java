113b76b61c61041c5ff20ebc97f5b19fccd28e47
Marisa <marisa_machado95@hotmail.com>
2017-12-26 19:29:49 +0000

    jogo

---
+++ app/src/main/java/pt/ipp/estgf/cmu_projeto/Fragments/Jogo.java
@@ -0,0 +1,15 @@
+package pt.ipp.estgf.cmu_projeto.Fragments;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import pt.ipp.estgf.cmu_projeto.R;
+
+public class Jogo extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo);
+    }
+}
