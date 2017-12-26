c81f7c76c4ecb91e9a2ac84a34d5063ff80c92d4
Marisa <marisa_machado95@hotmail.com>
2017-12-26 18:56:38 +0000

    jogo

--- app/src/main/java/pt/ipp/estgf/cmu_projeto/Activities/MenuJogo.java
+++ app/src/main/java/pt/ipp/estgf/cmu_projeto/Activities/MenuJogo.java
@@ -15,10 +15,10 @@ public class MenuJogo extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_jogo);
 
-        Button treino=(Button)findViewById(R.id.treino);
+        Button treino=findViewById(R.id.treino);
         treino.setOnClickListener(this);
 
-        Button competitivo=(Button)findViewById(R.id.competitivo);
+        Button competitivo=findViewById(R.id.competitivo);
         competitivo.setOnClickListener(this);
 
     }
