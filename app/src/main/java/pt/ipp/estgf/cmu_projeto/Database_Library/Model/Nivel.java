117ee18efa45823ed58f20131102eaac0a3e21f2
Marisa <marisa_machado95@hotmail.com>
2017-12-26 15:26:49 +0000

    base dados

--- app/src/main/java/pt/ipp/estgf/cmu_projeto/Database_Library/Model/Nivel.java
+++ app/src/main/java/pt/ipp/estgf/cmu_projeto/Database_Library/Model/Nivel.java
@@ -1,5 +1,9 @@
 package pt.ipp.estgf.cmu_projeto.Database_Library.Model;
 
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by Bernardino on 26/12/2017.
  */
@@ -32,5 +36,53 @@ public class Nivel {
 
     public void setNivel(int nivel) {
         this.nivel = nivel;
+    }
+
+
+    public boolean addNivel(SQLiteDatabase db) {
+        try {
+            db.execSQL("INSERT INTO " + NAME_TABLE + "(" + NIVEL + ")VALUES ('" + nivel + "');");
+            return true;
+        } catch (SQLException ex) {
+            db.close();
+            return false;
+        }
+    }
+
+    public boolean deleteNivel(SQLiteDatabase db) {
+        try {
+            db.execSQL("DELETE FROM " + NAME_TABLE + "WHERE " + ID_NIVEL + "=" + id_nivel + ";");
+            return true;
+        } catch (SQLException ex) {
+            db.close();
+            return false;
+        }
+    }
+
+    public boolean updateNivel(SQLiteDatabase db) {
+        try {
+            db.execSQL("UPDATE " + NAME_TABLE + "SET " + NIVEL  + " = '" + nivel + "' WHERE " + ID_NIVEL + "=" + id_nivel + ";");
+            return true;
+        } catch (SQLException ex) {
+            db.close();
+            return false;
+        }
+    }
+
+    public static Nivel getNivelById(SQLiteDatabase db, int id_nivel) {
+        try {
+            Cursor c = db.rawQuery("SELECT * FROM " + NAME_TABLE + "WHERE " + ID_NIVEL + "=" + id_nivel + ";", null);
+            Nivel nivel = null;
+
+            //se o cursor não estiver vazio e se estiver na primeira linha
+            if (c != null && c.moveToFirst()) {
+                nivel = new Nivel(c.getInt(0), c.getInt(1));
+            }
+            return nivel;
+
+        } catch (SQLException ex) {
+            db.close();
+            return null;
+        }
     }
 }
