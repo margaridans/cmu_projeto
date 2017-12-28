2cf651a2348bad85da9620bfd29891c91dc3aa3e
Marisa <marisa_machado95@hotmail.com>
2017-12-28 22:47:11 +0000

    estruturar

--- app/src/main/java/pt/ipp/estgf/cmu_projeto/Activities/Jogo/MainActivity.java
+++ app/src/main/java/pt/ipp/estgf/cmu_projeto/Activities/Jogo/MainActivity.java
@@ -1,85 +1,48 @@
 package pt.ipp.estgf.cmu_projeto.Activities.Jogo;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
+import pt.ipp.estgf.cmu_projeto.Activities.Login_Registo.Login;
 import pt.ipp.estgf.cmu_projeto.R;
 
 
-public class MainActivity extends AppCompatActivity  {
-/*
-    private Button btnSignUp;
-    private Login login;*/
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main_activity);
-/*
-        login = new Login(this);
-        login = login.open();
 
-        Button btnSignIn = findViewById(R.id.btnLogin);
-        //btnSignUp =  findViewById(R.id.buttonSignUP);
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
 
-                Intent intentSignUP = new Intent(getApplicationContext(),
-                        Registo.class);
-                startActivity(intentSignUP);
-            }
-        });
+        Button btnJogar = findViewById(R.id.btnJogar);
+        btnJogar.setOnClickListener(this);
+
+        Button btnDicas = findViewById(R.id.btnDicas);
+        btnDicas.setOnClickListener(this);
+
     }
-
-    public void btnJogarOnClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MenuJogo.class);
-        startActivity(intent);
-    }
-
-
-    public void signIn(View V) {
-        final Dialog dialog = new Dialog(MainActivity.this);
-        dialog.setContentView(R.layout.login);
-        dialog.setTitle("Login");
-        final EditText editTextUserName = dialog
-                .findViewById(R.id.editTextUserNameToLogin);
-        final EditText editTextPassword = dialog
-                .findViewById(R.id.editTextPasswordToLogin);
-
-        Button btnSignIn = dialog.findViewById(R.id.btnLogin);
-
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-                String userName = editTextUserName.getText().toString();
-                String password = editTextPassword.getText().toString();
-                String storedPassword = login
-                        .getSinlgeEntry(userName);
-                if (password.equals(storedPassword)) {
-                    Toast.makeText(MainActivity.this,
-                            "Login efetuado com sucesso", Toast.LENGTH_LONG)
-                            .show();
-                    dialog.dismiss();
-                } else {
-                    Toast.makeText(MainActivity.this,
-                            "Username e password não correspondem",
-                            Toast.LENGTH_LONG).show();
-                }
-            }
-
-        });
-
-        dialog.show();
-    }
-
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        login.close();
-    }
-    */
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnJogar) {
+            Intent intentJogar = new Intent(this, MenuJogo.class);
+            startActivity(intentJogar);
+        } //else if (v.getId() == R.id.btnDicas) {
+            //Intent intentDicas = new Intent (this, Dicas.class);
+            //startActivity(intentDicas);
+         else if (v.getId() == R.id.btnLogin) {
+            Intent intentLogin = new Intent(this, Login.class);
+            startActivity(intentLogin);
+        }
     }
 }
--- app/src/main/java/pt/ipp/estgf/cmu_projeto/Activities/Login_Registo/Login.java
+++ app/src/main/java/pt/ipp/estgf/cmu_projeto/Activities/Login_Registo/Login.java
@@ -12,6 +12,44 @@ import pt.ipp.estgf.database_library.Database.MyDbHelper;
 
 public class Login extends AppCompatActivity {
 
+    /*
+
+    public void signIn(View V) {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.login);
+        dialog.setTitle("Login");
+        final EditText editTextUserName = dialog
+                .findViewById(R.id.editTextUserNameToLogin);
+        final EditText editTextPassword = dialog
+                .findViewById(R.id.editTextPasswordToLogin);
+
+        Button btnSignIn = dialog.findViewById(R.id.btnLogin);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                String userName = editTextUserName.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String storedPassword = login
+                        .getSinlgeEntry(userName);
+                if (password.equals(storedPassword)) {
+                    Toast.makeText(MainActivity.this,
+                            "Login efetuado com sucesso", Toast.LENGTH_LONG)
+                            .show();
+                    dialog.dismiss();
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "Username e password não correspondem",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+        dialog.show();
+    }
+*/
 
     public SQLiteDatabase db;
     private final Context context;
