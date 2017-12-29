0eaa66455997bfca2fb643d580d4a3e828e0971a
Marisa <marisa_machado95@hotmail.com>
2017-12-29 01:33:22 +0000

    estruturar

--- app/src/main/java/pt/ipp/estgf/cmu_projeto/Interfaces/OnCategoriaSelectedItem.java
+++
@@ -1,9 +0,0 @@
-package pt.ipp.estgf.cmu_projeto.Interfaces;
-
-/**
- * Created by mjtm on 01-02-2017.
- */
-
-public interface OnCategoriaSelectedItem {
-    void onCategoriaselected(int idCategoria);
-}
